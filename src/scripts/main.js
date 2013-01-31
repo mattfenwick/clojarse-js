3b7a37245e8e1bf80a3b73b8b3f85ba1a095bec2
Matt Fenwick <mfenwick100@gmail.com>
2013-01-31 13:58:00 +0100

    get main page working:  tokenize input and display in html table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
-require(["libs/maybeerror", "libs/parsercombs"], function(me, pc) {
-    alert(JSON.stringify([me.zero, pc.item.parse("abc"), pc.item.parse([])]));
+var imports = [
+    "libs/maybeerror", "libs/parsercombs",
+    "app/tokens", "app/tokenizer"
+];
+
+require(imports, function(me, pc, tokens, tokenizer) {
+    $("#doit").click(function() {
+        var text = $("#clojure").val(),
+            result = tokenizer.scanner.parse(text),
+            out = $("#output");
+        out.empty();
+        out.append("<tr><th>token type</th><th>value</th></tr>");
+        if(result.status === 'success') {
+            result.value.result.map(function(t) {
+                out.append(["<tr><td>", t.tokentype, "</td><td>", t.value, "</td></tr>"].join(''));
+            });
+        } else if(result.status === 'error') {
+            alert('error: ' + JSON.stringify(result.value));
+        } else {
+            alert('failure: (no message provided)');
+        }
+    });
 });
