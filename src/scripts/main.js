e95754b32e65daff1e8a0d516aa4640cb62be4ee
Matt Fenwick <mfenwick100@gmail.com>
2013-02-05 13:37:31 +0100

    factor GUI a bit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,35 @@ var imports = [
     "app/ast_to_jstree", "app/regexizer"
 ];
 
+/* the 'views' */
+
+function renderTokens(elem, tokens) {
+    tokens.map(function(t) {
+        elem.append(["<tr><td>", t.tokentype, "</td><td>", t.value, "</td><td>", 
+                                t.meta.line, "</td><td>", t.meta.column, "</td></tr>"].join(''));
+    });
+}
+
+function failTokens(elem, result) {
+    result.tokens.map(function(t) {
+        elem.append(["<tr><td>", t.tokentype, "</td><td>", t.value, "</td><td>", 
+                                t.meta.line, "</td><td>", t.meta.column, "</td></tr>"].join(''));
+    });
+    var e = result.error;
+    elem.append(["<tr><td>", "ERROR", "</td><td>", e.message, '</td><td>', e.line, '</td><td>', e.column, '</td></tr>'].join(''));
+}
+
+function renderAST(elem, data) {
+    elem.jstree({"json_data": {"data": data},
+                 "plugins": ["themes", "json_data", "ui"]});
+}
+
+function failAST(elem, result) {
+    alert("parsing failed");
+}
+
+/* end views */
+
 require(imports, function(me, pc, tokens, tokenizer, ast, parser, a2jst, regs) {
     function filterJunk(tokens) {
         return tokens.filter(function(t) {
@@ -15,34 +44,24 @@ require(imports, function(me, pc, tokens, tokenizer, ast, parser, a2jst, regs) {
     $("#parse").click(function() {
         var text = $("#input").val(),
             result = regs.scanner(text),
-            out = $("#tokens"),
+            out = $("#tokens > tbody"),
             ast = $("#ast");
         out.empty();
         ast.empty();
-        out.append("<tr><th>token type</th><th>value</th><th>Line</th><th>Column</th></tr>");
         if(result.status === 'success') {
             var goodTokens = filterJunk(result.value);
-            goodTokens.map(function(t) {
-                out.append(["<tr><td>", t.tokentype, "</td><td>", t.value, "</td><td>", 
-                                        t.meta.line, "</td><td>", t.meta.column, "</td></tr>"].join(''));
-            });
+            renderTokens(out, goodTokens);
             var tree = parser.forms.parse(goodTokens);
             if(tree.status === 'success') { // what about if there's some tokens unconsumed?
                 var conned = tree.value.result.map(a2jst.convert);
-                ast.jstree({"json_data": {"data": conned},
-                                  "plugins" : [ "themes", "json_data", "ui" ]});
+                renderAST(ast, conned);
             } else {
-                alert("parsing failed");
+                failAST(ast, tree.value);
             }
         } else if(result.status === 'error') {
-            result.value.tokens.map(function(t) {
-                out.append(["<tr><td>", t.tokentype, "</td><td>", t.value, "</td><td>", 
-                                        t.meta.line, "</td><td>", t.meta.column, "</td></tr>"].join(''));
-            });
-            var e = result.value.error;
-            out.append(["<tr><td>", "ERROR", "</td><td>", e.message, '</td><td>', e.line, '</td><td>', e.column, '</td></tr>'].join(''));
+            failTokens(out, result.value);
         } else {
-            alert('failure: (no message provided)');
+            alert('unexpected failure: (no message provided)');
         }
     });
     
