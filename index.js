af965fe40504022b7ee5f990dc127cbc17bc8cc9
Matt Fenwick <mfenwick100@gmail.com>
2014-03-31 17:32:04 -0400

    npm packaging and dependencies

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+module.exports = {
+//    'parser'     : require('./lib/parser.js')     ,
+//    'treechecker': require('./lib/treechecker.js'),
+//    'validator'  : require('./lib/validator.js')
+};
+
