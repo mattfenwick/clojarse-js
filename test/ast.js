96b93c5af6be5a06fee805b9dd9c1ca0eef424d7
Matt Fenwick <mfenwick100@gmail.com>
2014-03-31 20:42:54 -0400

    get tests up and running with mocha

--- test/ast.js
+++ test/ast.js
@@ -1,34 +1,40 @@
-define(["app/ast"], function (AST) {
+"use strict";
 
-    return function() {
+var AST = require('../lib/ast'),
+    assert = require('assert');
+
+var module = describe,
+    test = it,
+    deepEqual = assert.deepEqual;
+
+
+module("ast", function() {
+
+    function astNode(type, value, meta) {
+        return {
+            type: 'astnode', 
+            asttype: type,
+            value: value,
+            meta: meta
+        };
+    }
     
-        module("ast");
-        
-        function astNode(type, value, meta) {
-            return {
-                type: 'astnode', 
-                asttype: type,
-                value: value,
-                meta: meta
-            };
-        }
-        
-        test("parser metadata", function() {
-            propEqual(astNode('number', 3, "i'm some metadata"), AST.number(3, "i'm some metadata"), 'number');
-            propEqual(astNode('nil', null, 14), AST.nil(14), 'nil');
-            propEqual(astNode('string', 'abc', 15), AST.string('abc', 15), 'string');
-            propEqual(astNode('symbol', 'def', 16), AST.symbol('def', 16), 'symbol');
-            propEqual(astNode('keyword', 'ghi', 17), AST.keyword('ghi', 17), 'keyword');
-            propEqual(astNode('boolean', true, 18), AST.boolean(true, 18), 'boolean');
-            propEqual(astNode('list', [], 39), AST.list([], 39), 'list');
-        });
-        
-        test("clojure metadata", function() {
-            propEqual(astNode('metadata', astNode('string', 'on', 'p'), 'hi'), AST.metadata(astNode('string', 'on', 'p'), 'hi'));
-        });
-        
-        test("macro forms", function() {
-            propEqual(astNode('unquotesplicing', 444, 'h'), AST.unquotesplicing(444, 'h'));
-        });
-    };
+    test("parser metadata", function() {
+        deepEqual(astNode('number', 3, "i'm some metadata"), AST.number(3, "i'm some metadata"), 'number');
+        deepEqual(astNode('nil', null, 14), AST.nil(14), 'nil');
+        deepEqual(astNode('string', 'abc', 15), AST.string('abc', 15), 'string');
+        deepEqual(astNode('symbol', 'def', 16), AST.symbol('def', 16), 'symbol');
+        deepEqual(astNode('keyword', 'ghi', 17), AST.keyword('ghi', 17), 'keyword');
+        deepEqual(astNode('boolean', true, 18), AST.boolean(true, 18), 'boolean');
+        deepEqual(astNode('list', [], 39), AST.list([], 39), 'list');
+    });
+    
+    test("clojure metadata", function() {
+        deepEqual(astNode('metadata', astNode('string', 'on', 'p'), 'hi'), AST.metadata(astNode('string', 'on', 'p'), 'hi'));
+    });
+    
+    test("macro forms", function() {
+        deepEqual(astNode('unquotesplicing', 444, 'h'), AST.unquotesplicing(444, 'h'));
+    });
 });
+
--- test/regexizer.js
+++ test/regexizer.js
@@ -1,4 +1,13 @@
-define(["libs/maybeerror", "app/tokens", "app/regexizer"], function (ME, Tokens, R) {
+"use strict";
+
+var ME = require('unparse-js').maybeerror,
+    Tokens = require('../lib/tokens'),
+    R = require('../lib/regexizer'),
+    assert = require('assert');
+
+var module = describe,
+    test = it,
+    deepEqual = assert.deepEqual;
 
     /* tests:
        1. token definitions
@@ -10,127 +19,123 @@ define(["libs/maybeerror", "app/tokens", "app/regexizer"], function (ME, Tokens,
        5. following chars
     */
     
-    return function() {
+module("regexizer", function() {
         
-        module("regexizer");
-        var scanner = R.scanner,
-            T       = Tokens.Token,
-            mPure   = ME.pure,
-            err     = ME.error;
+    var scanner = R.scanner,
+        T       = Tokens.Token,
+        mPure   = ME.pure,
+        err     = ME.error;
 
-        
-        test("token definitions -- punctuation", function() {
-            var ts = [
-                ['open-curly',   '{', 1, 1],
-                ['close-curly',  '}', 1, 2],
-                ['open-paren',   '(', 1, 3],
-                ['close-paren',  ')', 1, 4],
-                ['open-square',  '[', 1, 5],
-                ['close-square', ']', 1, 6],
-                ['open-fn',      '#(', 1, 7],
-                ['open-set',     '#{', 1, 9]
-            ].map(function(t) {
-                return T(t[0], t[1], {line: t[2], column: t[3]});
-            });
-                    
-            deepEqual(mPure(ts), scanner("{}()[]#(#{"), "punctuation");
+    
+    test("token definitions -- punctuation", function() {
+        var ts = [
+            ['open-curly',   '{', 1, 1],
+            ['close-curly',  '}', 1, 2],
+            ['open-paren',   '(', 1, 3],
+            ['close-paren',  ')', 1, 4],
+            ['open-square',  '[', 1, 5],
+            ['close-square', ']', 1, 6],
+            ['open-fn',      '#(', 1, 7],
+            ['open-set',     '#{', 1, 9]
+        ].map(function(t) {
+            return T(t[0], t[1], {line: t[2], column: t[3]});
         });
-        
-        test("token definitions -- all", function() {
-            var ts = [
-                ['number',       '123',   1, 1],
-                ['open-square',  '[',     1, 4],
-                ['nil',          'nil',   1, 5],
-                ['open-curly',   '{',     1, 8],
-                ['boolean',      'true',  1, 9],
-                ['open-paren',   '(',     1, 13],
-                ['symbol',       'abc',   1, 14],
-                ['close-square', ']',     1, 17],
-                ['boolean',      'false', 1, 18],
-                ['close-curly',  '}',     1, 23],
-                ['comment',      'oo',    1, 24],
-                ['space',        '\n',    1, 27],
-                ['open-set',     '#{',    2, 1],
-                ['string',       'blar',  2, 3],
-                ['regex',        'nu\\"\\hff',  2, 9],
-                ['space',        '  ',    2, 20],
-                ['keyword',      'non',   2, 22],
-                ['char',         'q',     2, 26],
-                ['close-paren',  ')',     2, 28],
-                ['open-fn',      '#(',    2, 29],
-                ['at-sign',      '@',     2, 31],
-                ['open-var',     "#'",    2, 32],
-                ['space',        ' ',     2, 34],
-                ['number',       '3.2',   2, 35],
-                ['space',        ' \n ',  2, 38],
-                ['unquote-splicing', '~@', 3, 2],
-                ['unquote',      '~',     3, 4],
-                ['syntax-quote', '`',     3, 5],
-                ['quote',        "'",     3, 6],
-                ['meta',         '^',     3, 7],
-                ['meta',         '#^',    3, 8],
-                ['char',         ' ',     3, 10]];
-            var tokens = ts.map(function(x) {
-                return T(x[0], x[1], {line: x[2], column: x[3]});
-            });
+                
+        deepEqual(mPure(ts), scanner("{}()[]#(#{"), "punctuation");
+    });
+    
+    test("token definitions -- all", function() {
+        var ts = [
+            ['number',       '123',   1, 1],
+            ['open-square',  '[',     1, 4],
+            ['nil',          'nil',   1, 5],
+            ['open-curly',   '{',     1, 8],
+            ['boolean',      'true',  1, 9],
+            ['open-paren',   '(',     1, 13],
+            ['symbol',       'abc',   1, 14],
+            ['close-square', ']',     1, 17],
+            ['boolean',      'false', 1, 18],
+            ['close-curly',  '}',     1, 23],
+            ['comment',      'oo',    1, 24],
+            ['space',        '\n',    1, 27],
+            ['open-set',     '#{',    2, 1],
+            ['string',       'blar',  2, 3],
+            ['regex',        'nu\\"\\hff',  2, 9],
+            ['space',        '  ',    2, 20],
+            ['keyword',      'non',   2, 22],
+            ['char',         'q',     2, 26],
+            ['close-paren',  ')',     2, 28],
+            ['open-fn',      '#(',    2, 29],
+            ['at-sign',      '@',     2, 31],
+            ['open-var',     "#'",    2, 32],
+            ['space',        ' ',     2, 34],
+            ['number',       '3.2',   2, 35],
+            ['space',        ' \n ',  2, 38],
+            ['unquote-splicing', '~@', 3, 2],
+            ['unquote',      '~',     3, 4],
+            ['syntax-quote', '`',     3, 5],
+            ['quote',        "'",     3, 6],
+            ['meta',         '^',     3, 7],
+            ['meta',         '#^',    3, 8],
+            ['char',         ' ',     3, 10]];
+        var tokens = ts.map(function(x) {
+            return T(x[0], x[1], {line: x[2], column: x[3]});
+        });
 
+        deepEqual(
+            mPure(tokens), 
+            scanner('123[nil{true(abc]false};oo\n#{"blar"#"nu\\"\\hff"  :non\\q)#(@#\' 3.2 \n ~@~`\'^#^\\space'), 
+            'all tokens');
+    });
+    
+    test("symbols", function() {
+        var syms = ['.', '%', '&', 'nil?', 'and#'];
+        syms.map(function(x) {
             deepEqual(
-                mPure(tokens), 
-                scanner('123[nil{true(abc]false};oo\n#{"blar"#"nu\\"\\hff"  :non\\q)#(@#\' 3.2 \n ~@~`\'^#^\\space'), 
-                'all tokens');
+                mPure([T('symbol', x, {line: 1, column: 1}), T('space', ' ', {line: 1, column: x.length + 1})]),
+                scanner(x + ' '),
+                'symbol ' + x);
         });
-        
-        test("symbols", function() {
-            var syms = ['.', '%', '&', 'nil?', 'and#'];
-            syms.map(function(x) {
-                propEqual(
-                    mPure([T('symbol', x, {line: 1, column: 1}), T('space', ' ', {line: 1, column: x.length + 1})]),
-                    scanner(x + ' '),
-                    'symbol ' + x);
-            });
-        });
-        
-        test("following chars", function() {
-            deepEqual(err({error: {message: 'invalid following characters', line: 1, column: 4, rest: 'abc'},
-                           tokens: []}),
-                      scanner("123abc"),
-                      'integer');
-            deepEqual(err({error: {message: 'invalid following characters', line: 1, column: 3, rest: '123'},
-                           tokens: []}),
-                      scanner("\\q123"),
-                      'char');
-            deepEqual(err({error: {message: 'invalid following characters', line: 1, column: 9, rest: '#{'},
-                           tokens: []}),
-                      scanner("\\newline#{"),
-                      'char -- long');
-        });
-        
-        test("error messages", function() {
-        
-            deepEqual(err({error: {message: 'end-of-string not found', line: 1, column: 4, rest: '"abc (derr 2 3)'}, 
-                           tokens: [T('number', '123', {line: 1, column: 1})]}), 
-                      scanner('123"abc (derr 2 3)'), 
-                'unclosed string literal');
+    });
+    
+    test("following chars", function() {
+        deepEqual(err({error: {message: 'invalid following characters', line: 1, column: 4, rest: 'abc'},
+                       tokens: []}),
+                  scanner("123abc"),
+                  'integer');
+        deepEqual(err({error: {message: 'invalid following characters', line: 1, column: 3, rest: '123'},
+                       tokens: []}),
+                  scanner("\\q123"),
+                  'char');
+        deepEqual(err({error: {message: 'invalid following characters', line: 1, column: 9, rest: '#{'},
+                       tokens: []}),
+                  scanner("\\newline#{"),
+                  'char -- long');
+    });
+    
+    test("error messages", function() {
+    
+        deepEqual(err({error: {message: 'end-of-string not found', line: 1, column: 4, rest: '"abc (derr 2 3)'}, 
+                       tokens: [T('number', '123', {line: 1, column: 1})]}), 
+                  scanner('123"abc (derr 2 3)'), 
+            'unclosed string literal');
 
-            deepEqual(err({error: {message: 'end-of-regex not found', line: 2, column: 1, rest: '#"abc (o m g)'}, 
-                           tokens: [T('space', ' \n', {line: 1, column: 1})]}), 
-                      scanner(' \n#"abc (o m g)'), 
-                'unclosed regex literal');
+        deepEqual(err({error: {message: 'end-of-regex not found', line: 2, column: 1, rest: '#"abc (o m g)'}, 
+                       tokens: [T('space', ' \n', {line: 1, column: 1})]}), 
+                  scanner(' \n#"abc (o m g)'), 
+            'unclosed regex literal');
 
-            deepEqual(err({error: {message: 'invalid following characters', line: 1, column: 7, rest: 'g'},
-                           tokens: [T('number', '123', {line: 1, column: 1}), T('space', ' ', {line: 1, column: 4})]}), 
-                      scanner("123 \\bg"), 
-                      "invalid character -- following by something ???");
-            
-            // don't know why I did propEqual instead ... just tired of seeing all those stupid prototype properties
-            propEqual(err({error: {message: 'invalid character escape', line: 1, column: 5, rest: '\\q" 123'},
-                           tokens: []}),
-                      scanner('"abc\\q" 123'),
-                      "invalid char escape");
-                        
-            // not sure if I can even hit this error -- what are some invalid chars in clojure ?? :  how about #abc or something?          
-    //            deepEqual(err({error: {}, tokens: []}), scanner(), 'no matching token found');
-        });
-    };
-
+        deepEqual(err({error: {message: 'invalid following characters', line: 1, column: 7, rest: 'g'},
+                       tokens: [T('number', '123', {line: 1, column: 1}), T('space', ' ', {line: 1, column: 4})]}), 
+                  scanner("123 \\bg"), 
+                  "invalid character -- following by something ???");
+        
+        deepEqual(err({error: {message: 'invalid character escape', line: 1, column: 5, rest: '\\q" 123'},
+                       tokens: []}),
+                  scanner('"abc\\q" 123'));
+                    
+        // not sure if I can even hit this error -- what are some invalid chars in clojure ?? :  how about #abc or something?          
+//            deepEqual(err({error: {}, tokens: []}), scanner(), 'no matching token found');
+    });
 });
+
