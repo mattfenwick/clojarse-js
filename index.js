32bd33b1f88a7282687129a075b65d1fee4c5f99
Matt Fenwick <mfenwick100@gmail.com>
2014-04-05 20:14:37 -0400

    make index.js parse stdin and write JSON to stdout

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 'use strict';
 
+var parser = require('./lib/parser'),
+    fs = require('fs');
+
+
+var input = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'});
+
+var output = JSON.stringify(parser.parse(input), null, 2);
+
+/*
+fs.writeFile('output', 
+    JSON.stringify(parser.parse(input), null, 2),
+    {'encoding': 'utf8'});
+*/
+process.stdout.write(output);
+
 
 module.exports = {
 //    'parser'     : require('./lib/parser.js')     ,
