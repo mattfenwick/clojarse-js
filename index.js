c49256bd572efc829e8c93b87ff692f9c99a5591
Matt Fenwick <mfenwick100@gmail.com>
2014-04-27 14:46:02 -0400

    fix problems in parser/tokens; sketch out tree traversal which runs token sub-parsers

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 'use strict';
 
-var parser = require('./lib/parser'),
+var parser = require('./lib/parser/structure'),
+    fullparser = require('./lib/parser/full'),
     fs = require('fs');
 
 
 var input = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'});
 
 var parsed = parser.parse(input);
+
+//console.log(Array.isArray(parsed));
+
+if ( !parsed.status ) {
+    parsed = parsed.map(fullparser.parseTokensInTree);
+}
 
 var output = JSON.stringify(parsed, null, 2);
 
