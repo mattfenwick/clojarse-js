febea36140489d48d66796a357364bded7d85721
Matt Fenwick <mfenwick100@gmail.com>
2014-05-04 19:32:46 -0400

    couple of unit tests for full parser.  also update npm test script to look in test/ subdirectories

---
+++ test/parser/full.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var F = require('../../lib/parser/full'),
+    assert = require('assert');
+
+var module = describe,
+    test = it,
+    deepEqual = assert.deepEqual;
+
+
+module("parser/full", function() {
+
+    var inp = '\\b \\u0041 \\backspace \\o101',
+        out = F.fullParse(inp);
+
+    test("char", function() {
+        deepEqual(out.body.length, 4);
+    });
+
+    test("char simple", function() {
+        deepEqual(out.body[0].value._name, 'simple');
+    });
+
+    test("char unicode", function() {
+        deepEqual(out.body[1].value._name, 'unicode');
+    });
+
+    test("char long", function() {
+        deepEqual(out.body[2].value._name, 'long');
+    });
+
+    test("char octal", function() {
+        deepEqual(out.body[3].value._name, 'octal');
+    });
+
+});
+
