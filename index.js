555a15f3db678a28f0ab6fad333f64f0cfaf38ad
Matt Fenwick <mfenwick100@gmail.com>
2014-05-08 20:20:27 -0400

    get ast up and running

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 
 var fullparser = require('./lib/parser/full'),
     specials = require('./lib/specials'),
+    A = require('./lib/ast'),
+    builder = require('./lib/astbuilder'),
     fs = require('fs');
 
 
 var input = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'});
 
 var parsed = fullparser.fullParse(input);
-parsed.fmap(specials.check_specials);
+var ast = parsed.fmap(builder.build);
+ast.fmap(specials.check_specials);
 
-var output = JSON.stringify(parsed, null, 2);
+// var output = JSON.stringify(parsed, null, 2) + JSON.stringify(ast, null, 2) + ast.fmap(A.dump).value;
+// var output = JSON.stringify(ast, null, 2) + '\n' + ast.fmap(A.dump).value;
+var output = ast.fmap(A.dump).mapError(JSON.stringify).value;
 
 /*
 fs.writeFile('output', 
