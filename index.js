b4f9e282bbbe09fb29bc5b0853b6ec4edbbcdd82
Matt Fenwick <mfenwick100@gmail.com>
2014-05-15 10:03:55 -0400

    get miscellaneous AST checks up and running.  use a couple of extra parameters instead of a monad stack

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var fullparser = require('./lib/parser/full'),
     specials = require('./lib/specials'),
     A = require('./lib/ast'),
+    M = require('./lib/misc'),
     builder = require('./lib/astbuilder'),
     fs = require('fs');
 
@@ -12,6 +13,12 @@ var input = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'});
 var parsed = fullparser.fullParse(input);
 var ast = parsed.fmap(builder.build);
 ast.fmap(specials.check_specials);
+ast.fmap(function(a) {
+    var state = {},
+        log = [];
+    M.traverse(a, state, log);
+    console.log('misc checks -- ' + JSON.stringify([state, log]));
+});
 
 // var output = JSON.stringify(parsed, null, 2) + JSON.stringify(ast, null, 2) + ast.fmap(A.dump).value;
 // var output = JSON.stringify(ast, null, 2) + '\n' + ast.fmap(A.dump).value;
