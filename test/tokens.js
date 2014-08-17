3f994bfdf1b06c307fedb902e86796d727c6a64d
Matt Fenwick <mfenwick100@gmail.com>
2014-08-17 12:20:46 -0400

    redo groups of CST node types
     - full implementation, in 1 module
     - split off `clojure` into new group by itself
    
    add `_end` key to CST nodes -- line/column after its last character

--- test/tokens.js
+++ test/tokens.js
@@ -29,20 +29,21 @@ module("tokens/char", function() {
 });
 
 module("tokens/integer", function() {
-    function int(sign, suffix, base, digits) {
+    function int(sign, suffix, base, digits, end) {
         return {
             '_name': 'integer', '_start': [1,1],
             'sign': sign, 'suffix': suffix,
-            'base': base, 'digits': digits
+            'base': base, 'digits': digits,
+            '_end': end
         };
     }
     var cases = [
-        ['0xdefN'   , int(null, 'N', 16, 'def')     ],
-        ['-077'     , int('-', null, 8, '77')       ],
-        ['+123'     , int('+', null, 10, '123')     ],
-        ['0'        , int(null, null, 10, '0')      ],
-        ['36r123zZ' , int(null, null, 36, '123zZ')  ],
-        ['40r888'   , int(null, null, 40, '888')    ]
+        ['0xdefN'   , int(null, 'N', 16, 'def', [1,7])     ],
+        ['-077'     , int('-', null, 8, '77', [1,5])       ],
+        ['+123'     , int('+', null, 10, '123', [1,5])     ],
+        ['0'        , int(null, null, 10, '0', [1,2])      ],
+        ['36r123zZ' , int(null, null, 36, '123zZ', [1,9])  ],
+        ['40r888'   , int(null, null, 40, '888', [1,7])    ]
     ];
     cases.map(function(c) {
         test('<' + c[0] + '>  ->  <' + JSON.stringify(c[1]) + '>', function() {
@@ -57,20 +58,20 @@ module("tokens/float", function() {
     function ex(sign, pow) {
         return {'sign': sign, 'power': pow};
     }
-    function float(sign, int, dec, exp, suff) {
+    function float(sign, int, dec, exp, suff, end) {
         return {
             '_name': 'float', '_start': [1,1], 
             'sign': sign, 'int': int,
             'decimal': dec, 'suffix': suff, 
-            'exponent': exp
+            'exponent': exp, '_end': end
         };
     }
     var cases = [
-        ['4M'           , float(null, '4', '', null, 'M')               ],
-        ['-0.'          , float('-', '0', '', null, null)               ],
-        ['18e37'        , float(null, '18', '', ex(null, '37'), null)   ],
-        ['+875.623e-22M', float('+', '875', '623', ex('-', '22'), 'M')  ],
-        ['01238M'       , float(null, '01238', '', null, 'M')           ]
+        ['4M'           , float(null, '4', '', null, 'M', [1,3])               ],
+        ['-0.'          , float('-', '0', '', null, null, [1,4])               ],
+        ['18e37'        , float(null, '18', '', ex(null, '37'), null, [1,6])   ],
+        ['+875.623e-22M', float('+', '875', '623', ex('-', '22'), 'M', [1,14]) ],
+        ['01238M'       , float(null, '01238', '', null, 'M', [1,7])           ]
     ];
     cases.map(function(c) {
         test('<' + c[0] + '>  ->  <' + JSON.stringify(c[1]) + '>', function() {
@@ -82,18 +83,18 @@ module("tokens/float", function() {
 });
 
 module("tokens/ratio", function() {
-    function ratio(sign, num, den) {
+    function ratio(sign, num, den, end) {
         return {
             '_name': 'ratio', '_start': [1,1], 
             'sign': sign, 'numerator': num,
-            'denominator': den 
+            'denominator': den, '_end': end
         };
     }
     var cases = [
-        ['0/0'      , ratio(null, '0', '0')     ],
-        ['01238/1'  , ratio(null, '01238', '1') ],
-        ['-198/202' , ratio('-', '198', '202')  ],
-        ['+18/34'   , ratio('+', '18', '34')    ]
+        ['0/0'      , ratio(null, '0', '0', [1,4])     ],
+        ['01238/1'  , ratio(null, '01238', '1', [1,8]) ],
+        ['-198/202' , ratio('-', '198', '202', [1,9])  ],
+        ['+18/34'   , ratio('+', '18', '34', [1,7])    ]
     ];
     cases.map(function(c) {
         test('<' + c[0] + '>  ->  <' + JSON.stringify(c[1]) + '>', function() {
@@ -120,25 +121,26 @@ module("tokens/number errors", function() {
 });
 
 module("tokens/ident", function() {
-    function ident(type, ns, name) {
+    function ident(type, ns, name, end) {
         return {
             '_name': type, '_start': [1,1], 
-            'ns': ns, 'name': name
+            'ns': ns, 'name': name,
+            '_end': end
         };
     }
-    function reserved(value) {
-        return {'_name': 'reserved', '_start': [1,1], 'value': value};
+    function reserved(value, end) {
+        return {'_name': 'reserved', '_start': [1,1], 'value': value, '_end': end};
     }
     
     var cases = [
-        ['nil'      , reserved('nil')               ],
-        ['x'        , ident('symbol' , null , 'x'  )],
-        [':x'       , ident('keyword', null , 'x'  )],
-        ['::x'      , ident('autokey', null , 'x'  )],
+        ['nil'      , reserved('nil', [1,4])            ],
+        ['x'        , ident('symbol' , null, 'x', [1,2])],
+        [':x'       , ident('keyword', null, 'x', [1,3])],
+        ['::x'      , ident('autokey', null, 'x', [1,4])],
         // TODO figure out where the problem is with the following case
         // I don't understand it
 //        [':///'     , ident('keyword', ''   , '//' )],
-        ['a:/b/c'   , ident('symbol' , 'a:' , 'b/c')]
+        ['a:/b/c'   , ident('symbol' , 'a:' , 'b/c', [1,7])]
     ];
     cases.map(function(c) {
         test('<' + c[0] + '>  ->  <' + JSON.stringify(c[1]) + '>', function() {
