f03f86601378d4b3378a42058b7dc09343f0934b
Matt Fenwick <mfenwick100@gmail.com>
2014-08-19 18:17:27 -0400

    start fixing tests

--- test/parser.js
+++ test/parser.js
@@ -81,3 +81,218 @@ module("parser/parseCst", function() {
 
 });
 
+module("parser/comment and whitespace", function() {
+    var cases = [
+        ["#! abc \n def", ' abc ', 'comment', P.comment],
+        ["; tuv\n xyz", ' tuv', 'comment', P.comment],
+        [", \r\f\n\t  qrs", ', \r\f\n\t  ', 'whitespace', P.whitespace]
+    ];
+    cases.map(function(c) {
+        test('<' + c[0] + '>', function() {
+            var parsed = c[3].parse(c[0], [1,1]);
+            deepEqual(parsed.status, 'success');
+            deepEqual(parsed.value.result._name, c[2]);
+            deepEqual(parsed.value.result.value, c[1].split(''));
+        });
+    });
+});
+
+module("parser/char", function() {
+    var cases = [
+        ['\\b', 'b'],
+        ['\\b""', 'b'],
+        ['\\  ', ' '],
+        ['\\\t ', '\t'],
+        ['\\\n,', '\n'],
+        ['\\blarghabag ', 'blarghabag'],
+        ['\\u123~[]', 'u123'],
+        ["\\a#'%{}", "a#'%", "only terminating macros and whitespace end a char"]
+    ];
+    cases.map(function(c) {
+        test('<' + c[0] + '>  ->  <' + c[1] + '>', function() {
+            var parsed = P.parse(c[0]);
+            deepEqual(parsed.status, 'success');
+            deepEqual(parsed.value.body[0]._name, 'char');
+            deepEqual(parsed.value.body[0].value, c[1]);
+        });
+    });
+});
+
+module("parser/number", function() {
+    var cases = [
+        ['4 ', '4'],
+        ["+3'x", "+3"],
+        ['-2xyz#{}', '-2xyz', 'ended by whitespace and macros'],
+        ['8????()', '8????']
+    ];
+    cases.map(function(c) {
+        test('<' + c[0] + '>  ->  <' + c[1] + '>', function() {
+            var parsed = P.parse(c[0]);
+            deepEqual(parsed.status, 'success');
+            deepEqual(parsed.value.body[0]._name, 'number');
+            deepEqual(parsed.value.body[0].value, c[1]);
+        });
+    });
+});
+
+module("parser/ident", function() {
+    var cases = [
+        ['x ', 'x'],
+        [':x,', ':x'],
+        ['::x\t', '::x'],
+        ['%234\n', '%234'],
+        ["x'#%[]", "x'#%"]
+    ];
+    cases.map(function(c) {
+        test('<' + c[0] + '>  ->  <' + c[1] + '>', function() {
+            var parsed = P.parse(c[0]);
+            deepEqual(parsed.status, 'success');
+            deepEqual(parsed.value.body[0]._name, 'ident');
+            deepEqual(parsed.value.body[0].value, c[1]);
+        });
+    });
+});
+
+module("parser/char", function() {
+    var cases = [
+        ['b'        , 'b'        , 'simple'  ],
+        ['u0041'    , '0041'     , 'unicode' ],
+        ['backspace', 'backspace', 'long'    ],
+        ['o101'     , '101'      , 'octal'   ],
+        ['o10'      , '10'       , 'octal'   ],
+        ['o7'       , '7'        , 'octal'   ]
+    ];
+    cases.map(function(c) {
+        test('<' + c[0] + '>  ->  <' + c[1] + '>', function() {
+            var p = P.parse(c[0]);
+            deepEqual(p.status, 'success');
+            deepEqual(p.value._name, 'char');
+            deepEqual(p.value.value, c[1]);
+            deepEqual(p.value.kind, c[2]);
+        });
+    });
+});
+
+module("parser/integer", function() {
+    function int(sign, suffix, base, digits, end) {
+        return {
+            '_name': 'integer', '_start': [1,1],
+            'sign': sign, 'suffix': suffix,
+            'base': base, 'digits': digits,
+            '_end': end
+        };
+    }
+    var cases = [
+        ['0xdefN'   , int(null, 'N', 16, 'def', [1,7])     ],
+        ['-077'     , int('-', null, 8, '77', [1,5])       ],
+        ['+123'     , int('+', null, 10, '123', [1,5])     ],
+        ['0'        , int(null, null, 10, '0', [1,2])      ],
+        ['36r123zZ' , int(null, null, 36, '123zZ', [1,9])  ],
+        ['40r888'   , int(null, null, 40, '888', [1,7])    ]
+    ];
+    cases.map(function(c) {
+        test('<' + c[0] + '>  ->  <' + JSON.stringify(c[1]) + '>', function() {
+            var p = P.parse(c[0]);
+            deepEqual(p.status, 'success');
+            deepEqual(p.value, c[1]);
+        });
+    });
+});
+
+module("parser/float", function() {
+    function ex(sign, pow) {
+        return {'sign': sign, 'power': pow};
+    }
+    function float(sign, int, dec, exp, suff, end) {
+        return {
+            '_name': 'float', '_start': [1,1], 
+            'sign': sign, 'int': int,
+            'decimal': dec, 'suffix': suff, 
+            'exponent': exp, '_end': end
+        };
+    }
+    var cases = [
+        ['4M'           , float(null, '4', '', null, 'M', [1,3])               ],
+        ['-0.'          , float('-', '0', '', null, null, [1,4])               ],
+        ['18e37'        , float(null, '18', '', ex(null, '37'), null, [1,6])   ],
+        ['+875.623e-22M', float('+', '875', '623', ex('-', '22'), 'M', [1,14]) ],
+        ['01238M'       , float(null, '01238', '', null, 'M', [1,7])           ]
+    ];
+    cases.map(function(c) {
+        test('<' + c[0] + '>  ->  <' + JSON.stringify(c[1]) + '>', function() {
+            var p = P.parse(c[0]);
+            deepEqual(p.status, 'success');
+            deepEqual(p.value, c[1]);
+        });
+    });
+});
+
+module("parser/ratio", function() {
+    function ratio(sign, num, den, end) {
+        return {
+            '_name': 'ratio', '_start': [1,1], 
+            'sign': sign, 'numerator': num,
+            'denominator': den, '_end': end
+        };
+    }
+    var cases = [
+        ['0/0'      , ratio(null, '0', '0', [1,4])     ],
+        ['01238/1'  , ratio(null, '01238', '1', [1,8]) ],
+        ['-198/202' , ratio('-', '198', '202', [1,9])  ],
+        ['+18/34'   , ratio('+', '18', '34', [1,7])    ]
+    ];
+    cases.map(function(c) {
+        test('<' + c[0] + '>  ->  <' + JSON.stringify(c[1]) + '>', function() {
+            var p = P.parse(c[0]);
+//            console.log(JSON.stringify(p));
+            deepEqual(p.status, 'success');
+            deepEqual(p.value, c[1]);
+        });
+    });
+});
+
+module("parser/number errors", function() {
+    var cases = [
+        ['01238',   ['octal digit', [1,5]] ],
+        ['4/z',     ['denominator', [1,3]] ]
+    ];
+    cases.map(function(c) {
+        test('<' + c[0] + '>  ->  ' + c[1], function() {
+            var p = P.parse(c[0]);
+            deepEqual(p.status, 'error');
+            deepEqual(p.value[p.value.length - 1], c[1]);
+        });
+    });
+});
+
+module("parser/ident", function() {
+    function ident(type, ns, name, end) {
+        return {
+            '_name': type, '_start': [1,1], 
+            'ns': ns, 'name': name,
+            '_end': end
+        };
+    }
+    function reserved(value, end) {
+        return {'_name': 'reserved', '_start': [1,1], 'value': value, '_end': end};
+    }
+    
+    var cases = [
+        ['nil'      , reserved('nil', [1,4])            ],
+        ['x'        , ident('symbol' , null, 'x', [1,2])],
+        [':x'       , ident('keyword', null, 'x', [1,3])],
+        ['::x'      , ident('autokey', null, 'x', [1,4])],
+        // TODO figure out where the problem is with the following case
+        // I don't understand it
+//        [':///'     , ident('keyword', ''   , '//' )],
+        ['a:/b/c'   , ident('symbol' , 'a:' , 'b/c', [1,7])]
+    ];
+    cases.map(function(c) {
+        test('<' + c[0] + '>  ->  <' + JSON.stringify(c[1]) + '>', function() {
+            var p = P.parse(c[0]);
+            deepEqual(p.status, 'success');
+            deepEqual(p.value, c[1]);
+        });
+    });
+});
+
