f10fcc8efb0570c13061985a356a29cd519d82a8
Matt Fenwick <mfenwick100@gmail.com>
2014-08-23 15:01:11 -0400

    tweak CLI module

--- main.js
+++ main.js
@@ -4,13 +4,16 @@ var index = require('./index'),
     fs = require('fs');
 
 
-var input = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'});
-
-var cstOrError = index.parseCst(input),
+var input = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'}),
+    cstOrError = index.parseCst(input),
     tree;
 
+function dump(x) {
+    return JSON.stringify(x, null, 2);
+}
+
 if ( process.argv[2] === 'cst' ) {
-    tree = cstOrError.fmap(JSON.stringify);
+    tree = cstOrError.fmap(dump);
 } else if ( process.argv[2] !== undefined ) {
     throw new Error('invalid arg -- can only be "cst": was ' + process.argv[2]);
 } else {
@@ -18,7 +21,7 @@ if ( process.argv[2] === 'cst' ) {
                      .fmap(index.ast.dump);
 }
 
-var o1 = tree.mapError(function(x) {return JSON.stringify(x, null, 2);}),
+var o1 = tree.mapError(dump),
     output = o1.value;
 
 process.stdout.write(output + "\n"); // TODO utf8?
